8a16b6ecb6eed970bef783bbf68e5cae42fd2aab
kroxii <kilona.soleis@gmail.com>
2025-06-23 14:09:17 +0200

    html and js start

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let price = 3.26;
+let cid = [
+    ["PENNY", 0.01],
+    ["NICKEL", 0.05],
+    ["DIME", 0.1],
+    ["QUARTER", 0.25],
+    ["ONE", 1.0],
+    ["FIVE", 5.0],
+    ["TEN", 10.0],
+    ["TWENTY", 20.0],
+    ["ONE HUNDRED", 100.0]
+];
